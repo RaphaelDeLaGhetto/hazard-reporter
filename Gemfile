c8616894ea3c091261294e29c3db1ed06e130745
Daniel Bidulock <daniel@capitolhill.ca>
2017-10-12 02:28:21 +0000

    Get changes for w6d2

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ group :development, :test do
 end
 
 group :production do
-  gem 'rails_12factor' 
+  gem 'rails_12factor'
   gem 'pg'
 end
+
+
--- config/database.rb
+++ config/database.rb
@@ -2,6 +2,7 @@ configure do
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
     ActiveRecord::Base.logger = Logger.new(STDOUT)
+    
     set :database, {
       adapter: "sqlite3",
       database: "db/db.sqlite3"
